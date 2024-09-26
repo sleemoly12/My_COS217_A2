3f62b52d7614dae77eb2912a629fb613ac6ac069
Siyeon Lee <sl4731@princeton.edu>
2024-09-25 22:49:51 -0400

    Tentatively finished Str_concat, double check in OH

--- stra.c
+++ stra.c
@@ -11,10 +11,9 @@ size_t Str_getLength(const char pcSrc[])
 /* copy src array to dst array */
 size_t Str_copy(const char pcSrc[], char pcDst[])
 {
-   size_t pcDstNew[];
+   int i = 0;
    assert(pcSrc != NULL);
    assert(pcDst != NULL);
-   int i = 0;
    while (pcSrc[i] != '\0') {
       pcDst[i] = pcSrc[i];
       i++;
@@ -23,4 +22,21 @@ size_t Str_copy(const char pcSrc[], char pcDst[])
    return pcDst;
 }
 
-/
+/* concatenate src array and dst array */
+size_t Str_concat(char pcSrc[], char pcDst[])
+{
+   int i = 0;
+   int j = 0;
+   assert(pcSrc != NULL);
+   assert(pcDst != NULL);
+   while (pcSrc[i] != '\0') {
+      pcDst[i] = pcSrc[i];
+      i++
+   }
+   while (pcDst[j] != '\0') {
+      pcDst[i] = pcDst[j];
+      j++
+   }
+   pcDst[i+j] = '\0';
+   return pcDst;
+}
