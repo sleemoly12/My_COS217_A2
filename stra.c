5bc2f6f4bbec8c841cbe1b6997656617cd6282e1
Siyeon Lee <sl4731@princeton.edu>
2024-09-26 16:38:12 -0400

    finished all but str_search, which passes only the boundary tests so far

--- stra.c
+++ stra.c
@@ -25,7 +25,8 @@ char* Str_copy(char pcDst[], const char pcSrc[])
    return pcDst;
 }
 
-/* concatenate src array and dst array */
+/* Return the modified destination array pcDst, which is 
+the destination array, pcDst, and source array, pcSrc, concatenated. */
 char* Str_concat(char pcDst[], const char pcSrc[])
 {
    size_t i = 0;
@@ -35,38 +36,68 @@ char* Str_concat(char pcDst[], const char pcSrc[])
    while (pcDst[i] != '\0') {
       i++;
    }
-   while (pcDst[j] != '\0') {
+   while (pcSrc[j] != '\0') {
       pcDst[i] = pcSrc[j];
       i++;
       j++;
    }
-   pcDst[i+j] = '\0';
+   pcDst[i] = '\0';
    return pcDst;
 }
 
-/* compare src array and dst array, return the int differences in letters */
+/* Returns 0 when pc1 and pc2 are equal, -1, or 1. Returns -1
+if pc1 is less than pc2. Returns 1 if pc1 is greater than pc2.*/
 int Str_compare(const char pc1[], const char pc2[])
 {
-   return 0;
-   /*
-   int i = 0;
+   size_t i = 0;
    assert(pc1 != NULL);
    assert(pc2 != NULL);
-   while (pc1[i] != '\0') {
-      if pc1[i] = pc2[i]{
-
+   while (pc1[i] != '\0' || pc2[i] != '\0') {
+      if (pc1[i] < pc2[i]) {
+         return -1;
       }
-      else if pc1[i] < pc2[i] {
-
+      else if (pc1[i] > pc2[i]) {
+         return 1;
       }
-      else if pc1[]
-     
+      i++;
    }
-   */
+   return 0;
 }
+
+/* Returns pointer to beginning of located substring needle,
+found in haystack, or NULL if substring is not found. 
+If needle is an empty string, return value is always haystack.*/
 
 char* Str_search(const char haystack[], const char needle[]) 
 {
+   size_t rememberMe = NULL;
+   size_t h = 0;
+   size_t n = 0;
+   assert(haystack != NULL);
+   assert(needle != NULL);
 
-return NULL;
+   /* If needle is an empty string, return haystack.*/
+   if (needle[0] == '\0') {
+      return haystack;
+   }
+   while (haystack[h] != '\0') {
+      if (haystack[h] == needle[n]){
+         if (rememberMe == NULL){
+            rememberMe = h;
+         }
+         h++;
+         n++;
+      }
+      else if (haystack[h] != needle[n]){
+         n == 0;
+         rememberMe == NULL;
+         h++;
+      }
+      else if (haystack[h] == '\0') {
+         return NULL;
+      }
+      else if (needle[n] == '\0') {
+         return rememberMe;
+      }
+   }
 }
