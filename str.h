12337cf4901cea88936666f9eb1ce62593b772ef
Siyeon Lee <sl4731@princeton.edu>
2024-09-26 15:24:08 -0400

    added correct comments, return types and arguments.

--- str.h
+++ str.h
@@ -1,14 +1,29 @@
 #ifndef STR_INCLUDED
 #define STR_INCLUDED
+#include <stddef.h>
 
-/* Return the length of the string pointer.*/
+/* name of the argument must be included in the comment */
+/* job, argument, return*/
+
+/* Return the length of the string pointer pcSrc, the argument. */
 /* only have to do it once for both programs!*/
-size_t Str_getLength(const char *pcSrc);
+size_t Str_getLength(const char pcSrc[]);
 
-/* Return the modified destination array, which is a copy of the source array. */
-size_t Str_copy(const char pcSrc[], char pcDst[])
+/* Return the modified destination array pcDst, which is a 
+copy of the source array pcSrc. */
+char* Str_copy(char pcDst[], const char pcSrc[]);
 
-/* Return the modified destination array, which is the source and destination array concatenated. */
-size_t Str_concat(char pcSrc[], char pcDst[])
+/* Return the modified destination array pcDst, which is 
+the destination array, pcDst, and source array, pcSrc, concatenated. */
+char* Str_concat(char pcDst[], const char pcSrc[]);
+
+/* Returns 0 when pc1 and pc2 are equal, -1, or 1. Returns -1
+if pc1 is less than pc2. Returns 1 if pc1 is greater than pc2.*/
+int Str_compare(const char pc1[], const char pc2[]);
+
+/* Returns pointer to beginning of located substring needle,
+found in haystack, or NULL if substring is not found. 
+If needle is an empty string, return value is always haystack.*/
+char* Str_search(const char haystack[], const char needle[]);
 
 #endif
