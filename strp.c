4f76effc031f0d7db8a4b98da372c5e7a97a9377
Siyeon Lee <sl4731@princeton.edu>
2024-10-01 15:38:26 -0400

    lots of errors but tentatively completed strp

--- strp.c
+++ strp.c
@@ -18,11 +18,15 @@ copy of the source array pcSrc. */
 
 char* Str_copy(char *pcDst, const char *pcSrc)
 {
+   /* Save the memory location of pcDst to final. */
    char *final = pcDst;
    assert(pcSrc != NULL);
    assert(pcDst != NULL);
+   /* Increment through and copy contents of pcSrc to pcDst*/
    while ((*pcDst++ = *pcSrc++) != '\0') {
    }
+   /* Return modified pcDst array, whose location is already
+   saved by final from earlier! */
    return final;
 }
 
@@ -45,69 +49,74 @@ char* Str_concat(char *pcDst, const char *pcSrc)
    while (*pcSrc != '\0') {
       *pcTemp++ = *pcSrc++;
    }
-   /* Add a new null terminator! */
+   /* Add a new null terminator!!!!!! */
    *pcTemp = '\0';
    return pcDst;
 }
 
-/* UNEDITED STARTING BELOW */
 
 /* Returns 0 when pc1 and pc2 are equal, -1, or 1. Returns -1
 if pc1 is less than pc2. Returns 1 if pc1 is greater than pc2.*/
 int Str_compare(const char *pc1, const char *pc2)
 {
-   size_t i = 0;
-   assert(pc1 != NULL);
-   assert(pc2 != NULL);
-   while (pc1[i] != '\0' || pc2[i] != '\0') {
-      if (pc1[i] < pc2[i]) {
+   assert(*pc1 != NULL);
+   assert(*pc2 != NULL);
+
+   while (*pc1 != '\0' || *pc2 != '\0') {
+      if (*pc1 < *pc2) {
          return -1;
       }
-      else if (pc1[i] > pc2[i]) {
+      else if (*pc1 > *pc2) {
          return 1;
       }
-      i++;
+      pc1++;
+      pc2++;
    }
    return 0;
 }
+
+/* UNEDITED STARTING BELOW */
+
 
 /* Returns pointer to beginning of located substring needle,
 found in haystack, or NULL if substring is not found. 
 If needle is an empty string, return value is always haystack.*/
 
-char* Str_search(const char haystack[], const char needle[]) 
+char* Str_search(const char *haystack, const char *needle) 
 {
-   const char* rememberMe = NULL;
-   size_t h = 0;
-   size_t n = 0;
-   assert(haystack != NULL);
-   assert(needle != NULL);
+   const char *rememberMe;
+   const char *h = haystack;
+   const char *n = needle;
+   const char *rememberMe;
 
-   /* If needle is an empty string, return haystack.*/
-   if (needle[0] == '\0') {
-      return haystack;
+   assert(*haystack != NULL);
+   assert(*needle != NULL);
+
+   /* If needle is an empty string, return haystack. */
+   if (*needle == '\0') {
+      return (char*)haystack;
    }
-   while (haystack[h] != '\0') {
-      if (haystack[h] == needle[n]){
-         if (n == 0){
-            rememberMe = &haystack[h];
-         }
-         h++;
-         n++;
-         if (needle[n] == '\0') {
-            return rememberMe;
-         }
-      }
-      else if (haystack[h] != needle[n]){
-         if (n > 0) {
-            h = h - n + 1;
-         }
-         else {
+
+
+   while (*h != 0){
+      /* If matching letter is found */
+      if (*h == *n) {
+         rememberMe = h;
+         while (*h != '\0' && *n != '\0' && *h == *n) {
             h++;
+            n++;
          }
-         n = 0;
+
+         /* If end of needle reached, match found */
+         if (*n == '\0') {
+            return (char*)rememberMe;
+         }
+         /* reset n pointer to beginning of needle */
+         n = needle; 
+         h = rememberMe + 1;
       }
+      /* Increment haystack */
+      else h++;
+        }
    }
-   /* If haystack reaches \0: */
    return NULL;
-}
